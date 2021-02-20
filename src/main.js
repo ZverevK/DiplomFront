1ade95b03e658c26c21090aaa7b125ed2d1e1e61
ZverevK <zverev.a.konstantin@yandex.ru>
2021-02-21 00:35:39 +0300

    check popup function

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
 import "./pages/main.css";
+import Popup from './scripts/Popup.js';
+
+(function() {
+    const regPopupWindow = document.querySelector('popup__registration');
+    const regPopupOnBtn = document.querySelector('popup__button_registration');
+    const regPopup = new Popup(regPopupWindow);
+    regPopup.setEventListener();
+    regPopupOnBtn.addEventListener('click', () => {
+        regPopup.open();
+    });
+
+})();
